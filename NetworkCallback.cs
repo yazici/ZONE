eb3d44d6227bc1744e35d511fc45c7c1218fe0d7
sw1pe <aidanstewart2000@gmail.com>
2019-08-15 18:50:02 -0500

    init

---
+++ NetworkCallback.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallback : Bolt.GlobalEventListener
+{
+
+    public override void SceneLoadLocalDone(string scene)
+    {
+       GameObject gameObject = BoltNetwork.Instantiate(BoltPrefabs.Player, new Vector3(0, 10, 0), Quaternion.identity);
+     
+
+    }
+  
+    
+}
